2a12b9509db506b988e779009b08b0acc967489c
Martin Kruger <martin@martinkruger.com>
2019-10-01 00:58:31 +0200

    Project, Task and addTask added

--- src/modules/ToDoList/ToDoList.js
+++ src/modules/ToDoList/ToDoList.js
@@ -1,5 +1,55 @@
+const projectInterface = (state) => ({
+  type: 'projectInterface',
+  add: () => state.add(state),
+});
+
+const addTask = (state) => ({
+  add: (item) => {
+    console.log
+    state.tasks.push(item);
+  },
+});
+
+const task = (title, desc, createDate, dueDate, priority, notes, checklist) => {
+  const proto = {
+    title,
+    desc,
+    createDate,
+    dueDate,
+    priority,
+    notes,
+    checklist,
+  };
+  return Object.assign(Object.create(proto), { title, desc });
+};
+
+const project = (title, desc) => {
+  const proto = {
+    title,
+    desc,
+    tasks: [],
+  };
+  return Object.assign(Object.create(proto), { title, desc }, addTask(proto));
+};
+
+const taskOutput = (item) => {
+  const proto = {
+    HTML() {
+      return `
+        <h1>
+          Item: ${item.title};
+        </h1>`;
+    },
+  };
+  return Object.assign(Object.create(proto), { item });
+};
+
 const ToDoList = () => {
-  console.log('ToDoList.js: ToDoList() run successfully');
+  const newProject = project('Main', 'School projects');
+  const newTask = task('Study', 'Finish chapter 4');
+  const output = taskOutput(newTask);
+  console.log(output.HTML());
+  newProject.add(newTask);
 };
 
 export default ToDoList;
