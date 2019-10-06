daeba8ed81cd9b2d68e503f381b7cb9b6c52a127
Martin Kruger <martin@martinkruger.com>
2019-10-06 16:06:28 +0200

    Added delete options to task overlay

--- src/modules/ToDoList/DOMController.js
+++ src/modules/ToDoList/DOMController.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-underscore-dangle */
+import * as Hammer from 'hammerjs';
+window.Hammer = Hammer.default;
 
 const DOMController = () => {
 
@@ -14,12 +16,27 @@ const DOMController = () => {
 
   };
 
-  const generateTask = (taskTitle, taskDesc) => {
+  const showTaskDeleteConfirmation = (index, task) => {
+    const taskOptionsConfirmContainer = task.getElementsByClassName('task-item-options-confirm-container');
+    taskOptionsConfirmContainer[0].classList.remove('hide-disable');
+  };
+
+  const hideTaskDeleteConfirmation = (index, task) => {
+    const taskOptionsConfirmContainer = task.getElementsByClassName('task-item-options-confirm-container');
+    taskOptionsConfirmContainer[0].classList.add('hide-disable');
+  };
+
+  const generateTask = (taskTitle, taskDesc, index) => {
     console.log('generateTask run:');
 
     // Main task container
     const taskContainer = document.createElement('div');
+    taskContainer.setAttribute('data-value-index', index);
+    console.log('TASK CONTAINER');
+    console.log(taskContainer);
     taskContainer.classList.add('task-container');
+    taskContainer.classList.add('bg4');
+    taskContainer.classList.add('box-shadow-1');
 
     // Project/category color bar
     const taskItemProjectColor = document.createElement('div');
@@ -72,11 +89,94 @@ const DOMController = () => {
     imgTaskItemTaskCompleted.setAttribute('src', '../src/assets/images/Flat_tick_icon.svg');
     taskItemTaskCompleted.appendChild(imgTaskItemTaskCompleted);
 
+    // Task options overlay container
+    const taskItemOptionsOverlayContainer = document.createElement('div');
+    taskItemOptionsOverlayContainer.classList.add('task-item-options-overlay-container');
+
+    // Task options moving container
+    const taskItemOptionsOverlay = document.createElement('div');
+    taskItemOptionsOverlay.classList.add('task-item-options-overlay');
+    taskItemOptionsOverlayContainer.appendChild(taskItemOptionsOverlay);
+
+    // Task options delete container
+    const taskItemOptionsDeleteContainer = document.createElement('div');
+    taskItemOptionsDeleteContainer.classList.add('task-item-options-delete-container');
+    taskItemOptionsOverlay.appendChild(taskItemOptionsDeleteContainer);
+
+    // Task options delete icon (rubbish bin) container
+    const taskItemOptionsDeleteIcon = document.createElement('div');
+    taskItemOptionsDeleteIcon.classList.add('task-item-options-delete-icon');
+    taskItemOptionsDeleteContainer.appendChild(taskItemOptionsDeleteIcon);
+
+    // Task options delete icon (rubbish bin) image
+    const imgDeleteTask = document.createElement('img');
+    imgDeleteTask.classList.add('img-delete-task');
+    imgDeleteTask.setAttribute('src', '../src/assets/images/Rubbish_bin.svg');
+    imgDeleteTask.setAttribute('alt', 'Delete Task');
+    imgDeleteTask.addEventListener('click', () => {
+      showTaskDeleteConfirmation(index, taskContainer);
+    });
+    taskItemOptionsDeleteIcon.appendChild(imgDeleteTask);
+
+    // Task options confirmation icons container
+    const taskItemOptionsConfirmContainer = document.createElement('div');
+    taskItemOptionsConfirmContainer.classList.add('task-item-options-confirm-container');
+    taskItemOptionsConfirmContainer.classList.add('hide-disable');
+    taskItemOptionsOverlay.appendChild(taskItemOptionsConfirmContainer);
+
+    // Task options confirm icon: No
+    const taskItemOptionsConfirmIconNo = document.createElement('div');
+    taskItemOptionsConfirmIconNo.classList.add('task-item-options-confirm-icon');
+    taskItemOptionsConfirmContainer.appendChild(taskItemOptionsConfirmIconNo);
+    
+    // Task options confirm icon img: No
+    const imgConfirmDeleteNo = document.createElement('img');
+    imgConfirmDeleteNo.classList.add('img-confirm-delete');
+    imgConfirmDeleteNo.setAttribute('src', '../src/assets/images/cross.svg');
+    imgConfirmDeleteNo.setAttribute('alt', 'Cancel Delete');
+    imgConfirmDeleteNo.addEventListener('click', () => {
+      hideTaskDeleteConfirmation(index, taskContainer);
+    });
+    taskItemOptionsConfirmIconNo.appendChild(imgConfirmDeleteNo);
+
+    // Task options confirm icon: Yes
+    const taskItemOptionsConfirmIconYes = document.createElement('div');
+    taskItemOptionsConfirmIconYes.classList.add('task-item-options-confirm-icon');
+    taskItemOptionsConfirmContainer.appendChild(taskItemOptionsConfirmIconYes);
+
+    // Task options confirm icon img: Yes
+    const imgConfirmDeleteYes = document.createElement('img');
+    imgConfirmDeleteYes.classList.add('img-confirm-delete');
+    imgConfirmDeleteYes.setAttribute('src', '../src/assets/images/tick.svg');
+    imgConfirmDeleteYes.setAttribute('alt', 'Confirm Delete');
+    imgConfirmDeleteYes.addEventListener('click', () => {
+      console.log(`CONFIRM CLICK: "YES". DELETE TASK Index: ${index}`);
+    });
+    taskItemOptionsConfirmIconYes.appendChild(imgConfirmDeleteYes);
+
+
+    taskContainer.appendChild(taskItemOptionsOverlayContainer);
+
     return taskContainer;
   };
 
   const generateTaskSettings = () => {
 
+  };
+
+  const addSwipeGesture = (el) => {
+    const swipeAction = new Hammer(el);
+    swipeAction.on('swipe', (ev) => {
+      toggleTaskOptions(el);
+    });
+  };
+
+  const taskSwipeController = () => {
+    const tasks = document.getElementsByClassName('task-container');
+    console.log(tasks);
+    for (let i = 0; i < tasks.length; i += 1) {
+      addSwipeGesture(tasks[i]);
+    }
   };
 
   const displayTasks = (projects) => {
@@ -87,10 +187,9 @@ const DOMController = () => {
     for (let i = 0; i < tasks.length; i += 1) {
       const title = tasks[i].getTitle();
       const desc = tasks[i].getDescription();
-
-      taskDisplay.appendChild(generateTask(title, desc));
-      // console.log(tasks[i].getTitle());
+      taskDisplay.appendChild(generateTask(title, desc, i));
     }
+    taskSwipeController();
   };
 
   const getTaskFromSettings = () => {
@@ -113,17 +212,27 @@ const DOMController = () => {
     taskSettingsDisplay.classList.add('hide-disable');
   };
 
+  const toggleTaskOptions = (el) => {
+    const overlay = el.getElementsByClassName('task-item-options-overlay');
+    overlay[0].classList.toggle('animate-task-item-options-overlay');
+  };
 
   const createEvents = () => {
     console.log('createEvents() run:');
-    const createNewTask = document.getElementById('settings-submit-new-task');
-    createNewTask.addEventListener('click', () => {
-      createNewTaskFromSettings();
-    });
+    const createNewTaskSubmit = document.getElementById('settings-submit-new-task');
+    createNewTaskSubmit.addEventListener('click', () => { createNewTaskFromSettings(); });
+
+    const newTaskButton = document.getElementById('add-new-task');
+    newTaskButton.addEventListener('click', () => { toggleTaskOptions(); });
   };
 
   const init = () => {
     createEvents();
+
+    /* Turn off settings screen while working, remove when done */
+    const taskSettingsDisplay = getTaskSettingsDisplay();
+    taskSettingsDisplay.classList.add('hide-disable');
+    
   };
 
   init();
